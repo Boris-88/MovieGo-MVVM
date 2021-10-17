69ec6d63fc80e53a78ae4bb4911100d27e9c47cc
Boris Zverik <Boris.zverik@gmail.com>
2021-10-17 12:29:00 +0300

    убрал закомиченный код

--- MoviesGo/MoviesGo/Service/ImageAPIService.swift
+++ MoviesGo/MoviesGo/Service/ImageAPIService.swift
@@ -3,32 +3,6 @@
 
 import UIKit
 
-// protocol ImageAPIServiceProtocol {
-//
-//    func getImage(posterPath: String?, complition: @escaping (Swift.Result<UIImage, Error>) -> ())
-// }
-//
-// final class ImageAPIService: ImageAPIServiceProtocol {
-//
-//    func getImage(posterPath: String?, complition: @escaping (Swift.Result<UIImage, Error>) -> ()) {
-//
-//        DispatchQueue.global().async {
-//            guard let posterPath = posterPath,
-//                  let url = URL(string: HTTPSettigs.imageHost + posterPath),
-//                  let imageData = try? Data(contentsOf: url),
-//                  let conteiner = UIImage(data: imageData)
-//            else {
-//                let error = NSError()
-//                complition(.failure(error))
-//                return
-//            }
-//            DispatchQueue.main.async {
-//                complition(.success(conteiner))
-//            }
-//        }
-//    }
-// }
-
 protocol ImageAPIServiceProtocol {
     func getPhoto(url: URL, completion: @escaping (Swift.Result<UIImage, Error>) -> Void)
 }
